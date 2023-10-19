098e04dae6ef2b614e076787eb8a6002db5d0a0d
benber86 <benoitb@tutanota.de>
2023-10-20 08:02:29 +1100

    Test rescue token

--- tests/test_admin_functions.py
+++ tests/test_admin_functions.py
@@ -29,7 +29,7 @@ def test_transfer_ownership(fn_isolation, voting_escrow_proxy, admin, alice):
     assert voting_escrow_proxy.admin() == alice
 
 
-def test_no_transfer_to_zero_address(
+def test_no_transfer_ownership_to_zero_address(
     fn_isolation, voting_escrow_proxy, admin, operator, alice
 ):
     assert voting_escrow_proxy.admin() == admin
@@ -37,3 +37,35 @@ def test_no_transfer_to_zero_address(
         voting_escrow_proxy.commit_transfer_ownership(
             brownie.ZERO_ADDRESS, {"from": admin}
         )
+
+
+def test_rescue_token_only_operator(
+    fn_isolation, voting_escrow_factory, curve_token, voting_escrow_proxy, alice
+):
+    curve_token.transfer(voting_escrow_proxy, 1e20, {"from": voting_escrow_factory})
+    with brownie.reverts():
+        voting_escrow_proxy.rescue_token(alice, 1e20, curve_token, {"from": alice})
+
+
+def test_rescue_token_not_to_zero_address(
+    fn_isolation, voting_escrow_factory, curve_token, voting_escrow_proxy, operator
+):
+    curve_token.transfer(voting_escrow_proxy, 1e20, {"from": voting_escrow_factory})
+    with brownie.reverts():
+        voting_escrow_proxy.rescue_token(
+            brownie.ZERO_ADDRESS, 1e20, curve_token, {"from": operator}
+        )
+
+
+def test_rescue_token(
+    fn_isolation,
+    voting_escrow_factory,
+    voting_escrow_proxy,
+    curve_token,
+    operator,
+    alice,
+):
+    curve_token.transfer(voting_escrow_proxy, 1e20, {"from": voting_escrow_factory})
+    operator_previous_balance = curve_token.balanceOf(operator)
+    voting_escrow_proxy.rescue_token(operator, 1e20, curve_token, {"from": operator})
+    assert curve_token.balanceOf(operator) == operator_previous_balance + 1e20
